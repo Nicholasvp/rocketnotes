7f0a276d2141def7eef709c7936ca7b5560927e2
nicholasvp <nicholasvpinheiro@gmail.com>
2023-02-08 19:27:18 -0300

    page New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,26 @@
+import { Container, Form } from './styles'
+
 import { NoteItem} from '../../components/NoteItem'
+import { Header} from '../../components/Header'
+import { ButtonText} from '../../components/ButtonText'
+import { Input} from '../../components/Input'
 
 export function New() {
     return (
-        <NoteItem 
-        isNew={true}
-        />
+        <Container>
+            <Header />
+            <main>
+                <Form>
+                    <header>
+                    <h1>Criar nota</h1>
+                    <ButtonText title="voltar"/>
+                    </header>
+
+                    <Input 
+                    placeholder="Título"
+                    />
+                </Form>
+            </main>
+        </Container>
     )
 }
